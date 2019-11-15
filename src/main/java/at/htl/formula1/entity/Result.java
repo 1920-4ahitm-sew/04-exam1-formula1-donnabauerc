bae3c9f285586d2cff4ce97b216f41e7840738d1
donnabauerc <christiandonnabauer@gmail.com>
2019-11-15 15:42:55 +0100

    add auto generated id

--- src/main/java/at/htl/formula1/entity/Result.java
+++ src/main/java/at/htl/formula1/entity/Result.java
@@ -12,6 +12,8 @@ public class Result {
     @Transient
     public int[] pointsPerPosition = {0, 25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @ManyToOne
     private Race race;
