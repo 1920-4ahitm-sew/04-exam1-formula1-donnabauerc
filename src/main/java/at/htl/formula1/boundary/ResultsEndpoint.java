b92634af6b0852809f23220faeb6fa6f0dd3c3d2
donnabauerc <christiandonnabauer@gmail.com>
2019-11-21 16:12:52 +0100

    correction of ResultsEndpoint

--- src/main/java/at/htl/formula1/boundary/ResultsEndpoint.java
+++ src/main/java/at/htl/formula1/boundary/ResultsEndpoint.java
@@ -1,18 +1,22 @@
 package at.htl.formula1.boundary;
 
+import at.htl.formula1.entity.Driver;
+import at.htl.formula1.entity.Race;
+import at.htl.formula1.entity.Team;
+import org.w3c.dom.ls.LSOutput;
+
+import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("results")
 public class ResultsEndpoint {
-
 
     @PersistenceContext
     EntityManager em;
@@ -24,20 +28,73 @@ public class ResultsEndpoint {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public JsonObject getPointsSumOfDriver(@QueryParam("name") String name) {
-        return null;
+        Driver driver = em
+                .createNamedQuery("Driver.findByName", Driver.class)
+                .setParameter("NAME", name)
+                .getSingleResult();
+        Long driverPoints = em
+                .createNamedQuery("Result.getPointsSumOfDriver", Long.class)
+                .setParameter("DRIVER", driver)
+                .getSingleResult();
+
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        builder.add("driver", driver.getName());
+        builder.add("points", driverPoints);
+        return builder.build();
     }
 
     /**
-     * @param id des Rennens
+     * @param country des Rennens
      * @return
      */
     @GET
-    public Response findWinnerOfRace(long id) {
-
-        return null;
+    @Path("winner/{country}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findWinnerOfRace(@PathParam("country") String country) {
+        Race race = em
+                .createNamedQuery("Race.findByCountry", Race.class)
+                .setParameter("COUNTRY", country)
+                .getSingleResult();
+        Driver winner = em
+                .createNamedQuery("Result.findWinnerOfRace", Driver.class)
+                .setParameter("RACE", race)
+                .getSingleResult();
+        return Response
+                .ok(winner)
+                .build();
     }
 
 
-    // Ergänzen Sie Ihre eigenen Methoden ...
+    /**
+     *
+     * @param teamname Name des Teams
+     * @return
+     */
+    @GET
+    @Path("raceswon")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findRacesWonByTeam(@QueryParam("team") String teamname) {
+        Team team = em
+                .createNamedQuery("Team.getByName", Team.class)
+                .setParameter("NAME", teamname)
+                .getSingleResult();
+        List<Race> racesWon = em
+                .createNamedQuery("Result.findRacesWonByTeam", Race.class)
+                .setParameter("TEAM", team)
+                .getResultList();
+        return Response.ok(racesWon).build();
+    }
+
+
+    @GET
+    @Path("all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Object[]> getPointsOfDrivers() {
+        List<Object[]> driverPoints = em
+                .createNamedQuery("Result.getPointsOfAllDrivers", Object[].class)
+                .getResultList();
+
+        return driverPoints;
+    }
 
 }
